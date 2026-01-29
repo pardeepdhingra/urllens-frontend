fe8938c360271dcf76b481519a51e0b9f74e135a
Pardeep <Pardeep@upflowy.com>
2026-01-29 21:07:28 +1100

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/about-pardeep`,
+      url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
